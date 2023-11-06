5c6f38d6bd0a1aa53c69fba6ce3beaa9f1261335
saphalpdyl <saphalpdyl@gmail.com>
2023-11-06 22:55:42 +0545

    ## Added some CSS to 'blog' page ## Tweaked `settings.py` to integrate static files

--- _12_new_blog/settings.py
+++ _12_new_blog/settings.py
@@ -111,4 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+# STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'static')
+]
